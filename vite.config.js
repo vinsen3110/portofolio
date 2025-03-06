cfe7136a11fafbda32da81c512a55a348089a5c9
vinsen3110 <bismoko3110@gmail.com>
2025-03-06 21:06:34 +0700

    add vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react';
+
 
 export default defineConfig({
     plugins: [
@@ -8,5 +10,7 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
+        [react()],
     ],
+
 });
