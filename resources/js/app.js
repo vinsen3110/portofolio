3dca0765d65e10d91b1561b93c7ed8e3166e3c28
vinsen3110 <bismoko3110@gmail.com>
2025-03-07 00:06:12 +0700

    test

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,8 @@
 import './bootstrap';
 import '../css/app.css';
 import { createRoot } from 'react-dom/client';
-import React from 'react';
-import App from './app';
+import React from 'react'; // Tambahkan ini!
+import App from './app'; // Pastikan ini benar, bukan './app'
 
 const root = createRoot(document.getElementById('app'));
 root.render(<App />);
-
